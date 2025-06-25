dc87d94d0542cc11e3aa51fef132a0ccaf9ae5b3
Giacca90 <giacca90mobile@gmail.com>
2025-06-25 14:49:50 +0200

    intento a medias de poner el init en el SSR (hay que mejorarlo, porque no funcionatodavía)

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,12 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+
 import { routes } from './app.routes';
 import { jwtRefreshInterceptor } from './interceptors/jwt-refresh.interceptor';
 import { jwtInterceptor } from './interceptors/jwt.interceptor';
 
+import { provideAppInitializer } from '@angular/core';
+import { InitService } from './services/init.service';
+
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch(), withInterceptors([jwtInterceptor, jwtRefreshInterceptor]))],
+	providers: [
+		provideRouter(routes),
+		provideClientHydration(),
+		provideHttpClient(withFetch(), withInterceptors([jwtInterceptor, jwtRefreshInterceptor])),
+
+		provideAppInitializer(() => {
+			const initService = inject(InitService);
+			return initService.carga();
+		}),
+	],
 };
